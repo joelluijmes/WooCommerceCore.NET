fdd9958b7ea6086a8a8a1a20d22f0426a94ae158
Joël Luijmes <joelluijmes@gmail.com>
2017-06-30 21:35:29 +0200

    add equality members

--- WooCommerceCore.NET/Entities/Products/Category.cs
+++ WooCommerceCore.NET/Entities/Products/Category.cs
@@ -32,5 +32,37 @@ namespace WooCommerceCore.NET.Models.Products
         public int Count { get; set; }
 
         public override string ToString() => Name;
+
+        private bool Equals(Category other) => Count == other.Count && string.Equals(Description, other.Description) && string.Equals(Display, other.Display) && Id == other.Id && MenuOrder == other.MenuOrder && string.Equals(Name, other.Name) && Parent == other.Parent && string.Equals(Slug, other.Slug);
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            return obj is Category && Equals((Category) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Count;
+                hashCode = (hashCode * 397) ^ (Description != null ? Description.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Display != null ? Display.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ Id;
+                hashCode = (hashCode * 397) ^ MenuOrder;
+                hashCode = (hashCode * 397) ^ (Name != null ? Name.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ Parent;
+                hashCode = (hashCode * 397) ^ (Slug != null ? Slug.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(Category left, Category right) => Equals(left, right);
+
+        public static bool operator !=(Category left, Category right) => !Equals(left, right);
     }
 }
