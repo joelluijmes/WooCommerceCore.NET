408ecf087a624da9ca92838fa836c570d5117d37
Joël Luijmes <joelluijmes@gmail.com>
2017-07-01 10:43:54 +0200

    OneTimeSetup to check server availability once

--- WooCommerceCore.NET.Tests/BaseTests.cs
+++ WooCommerceCore.NET.Tests/BaseTests.cs
@@ -8,7 +8,7 @@ namespace WooCommerceCore.NET.Tests
     {
         protected IJsonRestClient RestClient { get; private set; }
 
-        [SetUp]
+        [OneTimeSetUp]
         public async Task TestServerReachable()
         {
             var jsonClient = new JsonRestClient("https://bike.joelluijmes.nl/wp-json/wc/v2/", "ck_907833f04d2a0e269fc00e19ee4bc020bc25963f", "cs_f4ea0836e7ebf4a506168d7bc65d2f6785366829");
