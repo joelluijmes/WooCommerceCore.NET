7db3b33262cc598a0b124b5a477e3a3447c522fc
Joël Luijmes <joelluijmes@gmail.com>
2017-07-01 11:17:04 +0200

    moved configuration test to settings.json

--- WooCommerceCore.NET.Tests/BaseTests.cs
+++ WooCommerceCore.NET.Tests/BaseTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using NUnit.Framework;
 using WooCommerceCore.NET.REST;
 
@@ -7,11 +8,16 @@ namespace WooCommerceCore.NET.Tests
     public class BaseTests
     {
         protected IJsonRestClient RestClient { get; private set; }
+        protected IConfiguration Configuration { get; private set; }
 
         [OneTimeSetUp]
         public async Task TestServerReachable()
         {
-            var jsonClient = new JsonRestClient("https://bike.joelluijmes.nl/wp-json/wc/v2/", "ck_907833f04d2a0e269fc00e19ee4bc020bc25963f", "cs_f4ea0836e7ebf4a506168d7bc65d2f6785366829");
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("settings.json")
+                .Build();
+
+            var jsonClient = new JsonRestClient(Configuration["api_url"], Configuration["api_key"], Configuration["api_secret"]);
             RestClient = new WooCommerceRestClient(jsonClient);
 
             var response = await RestClient.GetJsonAsync("system_status");
